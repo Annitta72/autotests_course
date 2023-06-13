f07204eb8edff267cffdd73b523b48bf16611835
aa.egorova <aa.egorova@tensor.ru>
2023-06-13 16:35:45 +0500

    test 11

--- Homework11/task1.py
+++ Homework11/task1.py
@@ -9,3 +9,36 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+"""
+Тест №1
+1 Перейти на https://sbis.ru/
+2 Перейти в раздел "Контакты"
+3 Найти баннер Тензор, кликнуть по нему
+4 Перейти на https://tensor.ru/
+5 Проверить, что есть блок новости "Сила в людях"
+6 Перейдите в этом блоке в "Подробнее" и убедитесь, что открывается https://tensor.ru/about
+"""
+try:
+    driver.get('https://sbis.ru/')
+    contact = driver.find_element(By.CSS_SELECTOR, "[href='/contacts']")
+    contact.click()
+    sleep(2)
+    banner = driver.find_element(By.CSS_SELECTOR, "[href='https://tensor.ru/']")
+    banner.click()
+    driver.switch_to.window(driver.window_handles[1])
+    new = driver.find_element(By.CSS_SELECTOR, ".tensor_ru-Index__block4-content .tensor_ru-Index__card-title")
+    driver.execute_script("return arguments[0].scrollIntoView(true);", new)
+    sleep(2)
+    assert new.text == 'Сила в людях', 'Не соответствует текст новости'
+    about = driver.find_element(By.CSS_SELECTOR, ".tensor_ru-Index__block4-content [href='/about']")
+    sleep(1)
+    about.click()
+    assert driver.current_url == 'https://tensor.ru/about', 'Не верно открыт сайт'
+    sleep(2)
+    print('Тест №1 пройден!')
+finally:
+    driver.quit()
--- Homework11/task2.py
+++ Homework11/task2.py
@@ -7,4 +7,58 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Keys
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+"""
+Тест №2
+1 Авторизоваться на сайте https://fix-online.sbis.ru/
+2 Перейти в реестр Контакты
+3 Отправить сообщение самому себе
+4 Убедиться, что сообщение появилось в реестре
+5 Удалить это сообщение и убедиться, что удалили
+"""
+try:
+    driver.get('https://fix-online.sbis.ru/')
+    sleep(1)
+    login = driver.find_element(By.CSS_SELECTOR, '[name="Login"]')
+    login.send_keys('Игра', Keys.ENTER)
+    password = driver.find_element(By.CSS_SELECTOR, '[name="Password"]')
+    password.send_keys('Игра123', Keys.ENTER)
+    sleep(5)
+    contact = driver.find_element(By.CSS_SELECTOR, '[data-qa="Контакты"] [data-qa="NavigationPanels-Accordion__title"]')
+    contact.click()
+    sleep(2)
+    contact = driver.find_element(By.CSS_SELECTOR, '.NavigationPanels-SubMenu__headTitle')
+    contact.click()
+    sleep(1)
+    plus = driver.find_element(By.CSS_SELECTOR, '[data-qa="sabyPage-addButton"]')
+    plus.click()
+    sleep(2)
+    poisk = '.controls-StackTemplate__top-area-content .controls-Search__nativeField_caretEmpty'
+    name = driver.find_element(By.CSS_SELECTOR, poisk)
+    name.send_keys('Игрова Аня')
+    sleep(1)
+    my_name = driver.find_element(By.CSS_SELECTOR, '.person-BaseInfo')
+    my_name.click()
+    sleep(1)
+    text = driver.find_element(By.CSS_SELECTOR, '.textEditor_Viewer__Paragraph')
+    text.send_keys('Ура!!! Мои первые автотесты!')
+    text = driver.find_element(By.CSS_SELECTOR, '.textEditor_Viewer__Paragraph')
+    send = driver.find_element(By.CSS_SELECTOR, '.controls-StackTemplate__content-area [title="Отправить"]')
+    send.click()
+    sleep(1)
+    message = driver.find_element(By.CSS_SELECTOR, '.msg-dialogs-item')
+    assert message, 'Нет сообщения'
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(message)
+    action_chains.perform()
+    mes_del = driver.find_element(By.CSS_SELECTOR, '[data-qa="controls-itemActions__action deleteToArchive"]')
+    mes_del.click()
+    sleep(1)
+    assert message != '', 'Сообщение не удалено'
+    print('Тест №2 пройден!')
+finally:
+    driver.quit()
